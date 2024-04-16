3f8ef4bb5b4e995653579dcf78ce3057b11f5999
NeoBitose <ramadhanalif430@gmail.com>
2024-04-16 13:56:55 +0700

    feat(diagnosa): add diagnosa with AI model

--- app/Http/Controllers/DiagnosaController.php
+++ app/Http/Controllers/DiagnosaController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Http;
 
 class DiagnosaController extends Controller
 {
@@ -28,6 +30,7 @@ class DiagnosaController extends Controller
         $this->param['daun'] = $daun;
         $this->param['tangkai'] = $tangkai;
         $this->param['malai'] = $malai;
+        $this->param['diagnosa'] = [0=>null];
         // dd($this->param);
         return view('diagnosa', $this->param);
     }
@@ -81,6 +84,54 @@ class DiagnosaController extends Controller
     }
 
     public function diagnosa(Request $request){
+        $gejala = $request->gejala;
+        // dd($gejala);
+        $input = "";
+        for ($i=0; $i < count($gejala); $i++) { 
+            if ($i == 0) {
+                $input = $gejala[$i];
+            }else{
+                $input = $input." ".$gejala[$i];
+            }
+            
+        }
+        // dd($input);
+        // $client = new Client([
+        //     'base_uri' => 'http://127.0.0.1:5000/',
+        //     'timeout' => 5000.0,
+        // ]);
         
+        $response = Http::withUrlParameters([
+            'endpoint' => 'http://127.0.0.1:5000',
+            'gejala' => $input,
+        ])->acceptJson()->get('{+endpoint}/diagnosa/{gejala}');
+        
+        // Assuming the API returns JSON data
+        // $data = json_decode($response->getBody(), true);
+
+        $diags = DB::table('penyakit')
+                    ->where('nama_penyakit', $response->body())
+                    ->first();
+
+        $daun = DB::table('gejala')
+                    ->join('klasifikasi_gejala', 'gejala.klasifikasi_gejala_id', '=', 'klasifikasi_gejala.id_klasifikasi_gejala')
+                    ->where('nama_bagian', 'daun')
+                    ->get();
+        $tangkai = DB::table('gejala')
+                    ->join('klasifikasi_gejala', 'gejala.klasifikasi_gejala_id', '=', 'klasifikasi_gejala.id_klasifikasi_gejala')
+                    ->where('nama_bagian', 'tangkai')
+                    ->get();
+        $malai = DB::table('gejala')
+                    ->join('klasifikasi_gejala', 'gejala.klasifikasi_gejala_id', '=', 'klasifikasi_gejala.id_klasifikasi_gejala')
+                    ->where('nama_bagian', 'malai')
+                    ->get();
+        $this->param['daun'] = $daun;
+        $this->param['tangkai'] = $tangkai;
+        $this->param['malai'] = $malai;
+        $this->param['diagnosa'] = [$diags];
+
+        // dd($this->param);
+        // dd($this->param);
+        return view('diagnosa', $this->param);
     }
 }
--- resources/views/diagnosa.blade.php
+++ resources/views/diagnosa.blade.php
@@ -16,7 +16,7 @@
               <a class="nav-link active" aria-current="page" href="/">Beranda</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Deteksi Penyakit</a>
+              <a class="nav-link" href="/diagnosa">Deteksi Penyakit</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">Forum</a>
@@ -34,60 +34,37 @@
   </nav>
   <div class="hero-diagnosa bg-primary py-5">
     <div class="container">
-      <h1 class="text-white fw-bold text-center">Diagnosa Penyakit</h1>
+      <h1 class="text-white fw-bold text-center">Deteksi Penyakit</h1>
     </div>
   </div>
-
-  <div class="form py-5">
-    <form action="/hasil-diagnosa" method="POST" class="" enctype="multipart/form-data">
-      @csrf
-      <div class="accordion accordion-flush" id="accordionFlushExample">
-        <div class="container">
-          <div class="accordion-item daun">
-            <h2 class="accordion-header">
-              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                Gejala Pada Daun
-              </button>
-            </h2>
-            <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-              <div class="accordion-body">
-                <div class="row">
-                  @for ($i = 0; $i < count($daun); $i++)
-                  <div class="col-6">
-                    <div class="form-check">
-                      <input class="form-check-input" name="gejala{{$daun[$i]->id_gejala}}" type="checkbox" value="{{$daun[$i]->gejala}}" id="gejala{{$daun[$i]->id_gejala}}">
-                      <label class="form-check-label" for="gejala{{$daun[$i]->id_gejala}}">
-                        {{$daun[$i]->gejala}}
-                      </label>
-                    </div>
-                  </div>
-                  @endfor
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="accordion accordion-flush" id="accordionFlushExample1">
-        <div class="bg-success">
+  <div class="main-deteksi">
+    <div class="form py-5">
+      <form action="/hasil-diagnosa" method="POST" class="" enctype="multipart/form-data">
+        @csrf
+        <div class="accordion accordion-flush" id="accordionFlushExample">
           <div class="container">
-            <div class="accordion-item tangkai">
+            <div class="accordion-item daun">
               <h2 class="accordion-header">
                 <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                  data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                  Gejala Pada Tangkai
+                  data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                  Gejala Pada Daun
                 </button>
               </h2>
-              <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample1">
+              <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
                 <div class="accordion-body">
                   <div class="row">
-                    @for ($i = 0; $i < count($tangkai); $i++)
+                    @php
+                        $no = 0
+                    @endphp
+                    @for ($i = 0; $i < count($daun); $i++)
+                    @php
+                        $no++
+                    @endphp
                     <div class="col-6">
                       <div class="form-check">
-                        <input class="form-check-input" name="gejala{{$tangkai[$i]->id_gejala}}" type="checkbox" value="{{$tangkai[$i]->gejala}}" id="gejala{{$tangkai[$i]->id_gejala}}">
-                        <label class="form-check-label" for="gejala{{$tangkai[$i]->id_gejala}}">
-                          {{$tangkai[$i]->gejala}}
+                        <input class="form-check-input" name="gejala[]" type="checkbox" value="{{$daun[$i]->gejala}}" id="gejala{{$daun[$i]->id_gejala}}">
+                        <label class="form-check-label" for="gejala{{$daun[$i]->id_gejala}}">
+                          {{$daun[$i]->gejala}}
                         </label>
                       </div>
                     </div>
@@ -98,61 +75,106 @@
             </div>
           </div>
         </div>
-      </div>
-      <div class="accordion accordion-flush" id="accordionFlushExample2">
-        <div class="container">
-          <div class="accordion-item malai">
-            <h2 class="accordion-header">
-              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-                Gejala Pada Malai
-              </button>
-            </h2>
-            <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample2">
-              <div class="accordion-body">
-                <div class="row">
-                  @for ($i = 0; $i < count($malai); $i++)
-                  <div class="col-6">
-                    <div class="form-check">
-                      <input class="form-check-input" name="gejala{{$malai[$i]->id_gejala}}" type="checkbox" value="{{$malai[$i]->gejala}}" id="gejala{{$malai[$i]->id_gejala}}">
-                      <label class="form-check-label" for="gejala{{$malai[$i]->id_gejala}}">
-                        {{$malai[$i]->gejala}}
-                      </label>
+        <div class="accordion accordion-flush" id="accordionFlushExample1">
+          <div class="bg-success">
+            <div class="container">
+              <div class="accordion-item tangkai">
+                <h2 class="accordion-header">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                    Gejala Pada Tangkai
+                  </button>
+                </h2>
+                <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample1">
+                  <div class="accordion-body">
+                    <div class="row">
+                      @for ($i = 0; $i < count($tangkai); $i++)
+                      @php
+                        $no++
+                      @endphp
+                      <div class="col-6">
+                        <div class="form-check">
+                          <input class="form-check-input" name="gejala[]" type="checkbox" value="{{$tangkai[$i]->gejala}}" id="gejala{{$tangkai[$i]->id_gejala}}">
+                          <label class="form-check-label" for="gejala{{$tangkai[$i]->id_gejala}}">
+                            {{$tangkai[$i]->gejala}}
+                          </label>
+                        </div>
+                      </div>
+                      @endfor
                     </div>
                   </div>
-                  @endfor
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <div class="container pt-5">
-        <button class="diagnosa-btn">Kirim</button>
-      </div>
-    </form>
-  </div>
-  <div class="container py-5">
-    <div class="card diagnosa-card">
-      <div class="card-header bg-primary py-3">
-        <h3 class="text-white text-center fw-bold">
-          Hasil Prediksi Penyakit dan Diagnosa
-        </h3>
-      </div>
-      <div class="card-body text-primary hasil-diagnosa p-4">
-        <h1 class="card-title fw-bold">Penyakit Tungro</h1>
-        <p class="card-text">
-          Penyakit tungro berasal dari virus yang ditularkan oleh wereng hijau, yaitu Nephotettix impicticeps. Hama wereng bisa mengakibatkan tanaman padi terserang virus setelah tanaman terhisap oleh hama wereng.
-        </p>
-        <h3 class="card-title fw-bold">Pengendalian Teknis</h3>
-        <p class="card-text">
-          Menanam padi tahan wereng seperti Kelara        </p>
-        <h3 class="card-title fw-bold">Pengendalian Gejala</h3>
-        <p class="card-text">
-          Semua rumpun tanaman yang sudah terjangkit virus tungro segera diambil, dipisahkan dari rumpun tanaman lain, dan dimusnahkan        </p>
+        <div class="accordion accordion-flush" id="accordionFlushExample2">
+          <div class="container">
+            <div class="accordion-item malai">
+              <h2 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                  data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                  Gejala Pada Malai
+                </button>
+              </h2>
+              <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample2">
+                <div class="accordion-body">
+                  <div class="row">
+                    @for ($i = 0; $i < count($malai); $i++)
+                    @php
+                        $no++
+                    @endphp
+                    <div class="col-6">
+                      <div class="form-check">
+                        <input class="form-check-input" name="gejala[]" type="checkbox" value="{{$malai[$i]->gejala}}" id="gejala{{$malai[$i]->id_gejala}}">
+                        <label class="form-check-label" for="gejala{{$malai[$i]->id_gejala}}">
+                          {{$malai[$i]->gejala}}
+                        </label>
+                      </div>
+                    </div>
+                    @endfor
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="container pt-5">
+          <button class="diagnosa-btn">Kirim</button>
+        </div>
+      </form>
+    </div>
+    {{-- @php
+        dd($diagnosa);
+    @endphp --}}
+    @if ($diagnosa[0] != null)
+    <div class="container py-5">
+      <div class="card diagnosa-card">
+        <div class="card-header bg-primary py-3">
+          <h3 class="text-white text-center fw-bold">
+            Hasil Prediksi Penyakit dan Diagnosa
+          </h3>
+        </div>
+        <div class="card-body text-primary hasil-diagnosa p-4">
+          <h1 class="card-title fw-bold">Penyakit {{$diagnosa[0]->nama_penyakit}}</h1>
+          <p class="card-text">
+            {{$diagnosa[0]->definisi}} {{$diagnosa[0]->penyebab}}
+          </p>
+          <h3 class="card-title fw-bold">Pengendalian Teknis</h3>
+          <p class="card-text">
+            {{$diagnosa[0]->pengendalian_teknis}}      
+          </p>
+          <h3 class="card-title fw-bold">Pengendalian Gejala</h3>
+          <p class="card-text">
+            {{$diagnosa[0]->pengendalian_gejala}}
+          </p>
+        </div>
       </div>
     </div>
+    @endif
   </div>
+  
+  
   <div class="copyright text-center w-100 bg-dark py-4">
     <h4 class="text-white m-0">MAPAN Copyright © 2024 | Made With Love</h4>
   </div>
