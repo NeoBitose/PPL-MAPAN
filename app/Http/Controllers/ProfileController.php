780a708085e532ee3cc683d6e60e7a287db81b3e
NeoBitose <ramadhanalif430@gmail.com>
2024-05-01 22:42:09 +0700

    feat(profile): add profile management

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -6,11 +6,106 @@ use App\Http\Requests\ProfileUpdateRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
 
 class ProfileController extends Controller
 {
+    public function show_admin(){
+        $id = auth()->user()->id;
+        $data = DB::table('users')
+                    ->where('id', $id)
+                    ->select('id', 'name', 'email', 'role')
+                    ->first();
+        if ($data->role == 'user') {
+            return redirect()->back();
+        }
+        dd($data);
+    }
+
+    public function show_user(){
+        $id = auth()->user()->id;
+        $data = DB::table('users')
+                    ->where('id', $id)
+                    ->select('id', 'name', 'email', 'role')
+                    ->first();
+        if ($data->role == 'admin') {
+            return redirect()->back();
+        }
+        dd($data);
+    }
+
+    public function edit_admin(){
+        $id = auth()->user()->id;
+        $data = DB::table('users')
+                    ->where('id', $id)
+                    ->select('id', 'name', 'email', 'role')
+                    ->first();
+        if ($data->role == 'user') {
+            return redirect()->back();
+        }
+        dd($data);
+    }
+
+    public function edit_user(){
+        $id = auth()->user()->id;
+        $data = DB::table('users')
+                    ->where('id', $id)
+                    ->select('id', 'name', 'email', 'role')
+                    ->first();
+        if ($data->role == 'admin') {
+            return redirect()->back();
+        }
+        dd($data);
+    }
+
+    public function update_admin(Request $request, string $id){
+        $request->validate([
+            'name' => 'unique:users,name',
+            'email' => 'unique:users,email',
+            'password' => 'min:8',
+            'confirm_password' => 'same:password'
+        ]);
+
+        if ($request->password == "") {
+            DB::table('users')->where('id', $id)->update([
+                'name' => $request->name,
+                'email' => $request->email,
+            ]);
+        } else {
+            DB::table('users')->where('id', $id)->update([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => $request->password
+            ]);
+        }
+        
+    }
+
+    public function update_user(Request $request, string $id){
+        $request->validate([
+            'name' => 'unique:users,name',
+            'email' => 'unique:users,email',
+            'password' => 'min:8',
+            'confirm_password' => 'same:password'
+        ]);
+
+        if ($request->password == "") {
+            DB::table('users')->where('id', $id)->update([
+                'name' => $request->name,
+                'email' => $request->email,
+            ]);
+        } else {
+            DB::table('users')->where('id', $id)->update([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => $request->password
+            ]);
+        }
+        
+    }
+
     /**
      * Display the user's profile form.
      */
--- routes/web.php
+++ routes/web.php
@@ -20,15 +20,25 @@ Route::get('/', [IndexController::class, 'index']);
 Route::resource('/diagnosa', DiagnosaController::class);
 Route::post('/hasil-diagnosa', [DiagnosaController::class, 'diagnosa']);
 
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/profile-admin', [ProfileController::class, 'show_admin']);
+    Route::get('/profile-user', [ProfileController::class, 'show_user']);
+    Route::get('/edit-admin/{id}', [ProfileController::class, 'edit_admin']);
+    Route::get('/edit-user/{id}', [ProfileController::class, 'edit_user']);
+    Route::get('/update-admin/{id}', [ProfileController::class, 'update_admin']);
+    Route::get('/update-user/{id}', [ProfileController::class, 'update_user']);
+
+    
+
 });
 
 require __DIR__.'/auth.php';
